988be20fa99cef462aca01d32f44369061f40f22
Aleksei Artemev <kironeum@mail.ru>
2020-12-17 20:05:17 +0900

    added pages and modified style.css

--- 3.webPython/app.py
+++ 3.webPython/app.py
@@ -12,6 +12,22 @@ def index_page():
     """
     return render_template("index.html")
 
+@app.route('/product')
+def product_page():
+    """
+    функция логики страницы
+    Product page
+    """
+    return render_template("product.html")
+
+@app.route('/contact')
+def contact_page():
+    """
+    функция логики страницы
+    Contact page
+    """
+    return render_template("contact.html")
+
 # конструкция запуска приложения 
 if __name__ == "__main__":
     app.run(debug=True)
