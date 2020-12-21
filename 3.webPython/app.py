4b917ed40d1cbdd32ca38574e490053a637a528d
Aleksei Artemev <kironeum@mail.ru>
2020-12-21 20:10:40 +0900

    modified 3.webpython

--- 3.webPython/app.py
+++ 3.webPython/app.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import hashlib
 
 # экземпляр класса Flask
 app = Flask(__name__)
 
+def hashing(pwd, num_char):
+    """
+    Функция шифрования
+    """
+    # 2. кодирование
+    byte_str = pwd.encode()
+
+    # 3. шифрование
+    hash_str = hashlib.sha256(byte_str)
+
+    # 4. преобразование в строку hex-числа (шестнадцатиричного числа) 
+    if num_char == '-':
+        hex_str = hash_str.hexdigest()
+    else:
+        hex_str = hash_str.hexdigest()[:int(num_char)]
+    
+    return hex_str
+
+ 
 # декоратор маршрутизации 
 @app.route('/')
 def index_page():
@@ -12,13 +32,21 @@ def index_page():
     """
     return render_template("index.html")
 
-@app.route('/product')
+@app.route('/product', methods=['get', 'post'])
 def product_page():
     """
     функция логики страницы
     Product page
     """
-    return render_template("product.html")
+    message = ''
+    if request.method == 'POST':
+        site_name = request.form.get('site')
+        password = request.form.get('pwd')
+        num_char = request.form.get('num')
+
+        message = hashing(site_name + password, 10)
+
+    return render_template("product.html", message=message)
 
 @app.route('/contact')
 def contact_page():
@@ -32,3 +60,5 @@ def contact_page():
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+#  СТИЛИЗОВАТЬ home PAGE 
